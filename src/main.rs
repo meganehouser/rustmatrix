a9ef430cafc1f2caede62ea59d12af17eec3ac99
meganehouser <sleepy.st818@gmail.com>
2019-10-28 09:47:10 +0900

    set background color

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     let size = siv.screen_size();
     let mut theme = Theme::default();
     theme.palette.set_color("view", Color::TerminalDefault );
+    theme.palette.set_color("background", Color::TerminalDefault );
     siv.set_theme(theme);
 
     siv.add_fullscreen_layer(GreenCodeView::new(1, size));
